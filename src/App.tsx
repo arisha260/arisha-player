b281295f970733fed90338340ad948c3b566c069
Aleksey Morin <aryoshas@yandex.ru>
2026-01-10 14:55:46 +0300

    update: FS hook

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ export default function App() {
 
   const { isPlaying, currentTime, duration, progress, togglePlay, setVideoTimeByPercent, bufferedPct, isWaiting, toggleTimeSkip } = useVideo({ videoRef, isTouch });
   const { barRef, isMuted, videoVolume, setVideoVolume, toggleVolume } = useSound({ videoRef });
-  const { isFullscreen, handleFullScreen } = useFullScreen({ playerRef });
+  const { isFullscreen, handleFullScreen } = useFullScreen({ playerRef, videoRef });
   const { activeControls, handlePlayerClick, handleBottomHoverStart, handleBottomHoverEnd } = usePlayerControls({ playerRef, togglePlay, isTouch, isOpenModal });
 
   const { settings, updateSettings } = usePlayerSettings({ videoRef });
@@ -47,8 +47,7 @@ export default function App() {
           className="video"
           src="https://arisha-player.hb.ru-msk.vkcloud-storage.ru/%D0%AD%D0%B4%D0%B8%D1%82%20%D0%97%D0%BB%D0%BE%D0%B1%D0%B8%D0%BD%D1%83.mp4"
           onClick={() => handlePlayerClick()}
-          playsInline={false}
-          webkit-playsinline
+          playsInline
           controls={false}
         />
 
--- src/hooks/useFullScreen.ts
+++ src/hooks/useFullScreen.ts
@@ -2,22 +2,28 @@ import { useEffect, useState } from "react";
 
 interface Props {
     playerRef: React.RefObject<HTMLDivElement | null>;
+    videoRef: React.RefObject<HTMLVideoElement | null>;
 }
 
-export const useFullScreen = ({playerRef}: Props) => {
+export const useFullScreen = ({playerRef, videoRef}: Props) => {
 
     const [isFullscreen, setIsFullscreen] = useState(false);
 
     const handleFullScreen = () => {
         const player = playerRef.current;
-        if (!player) return;
+        const video = videoRef.current;
+        if (!player || !video) return;
+
+        if (video.webkitRequestFullscreen) {
+            video.webkitRequestFullscreen();
+            return;
+        }
 
         if (!document.fullscreenElement) {
             player.requestFullscreen();
             setIsFullscreen(true);
         } else {
             document.exitFullscreen();
-
             setIsFullscreen(false);
         }
     };
